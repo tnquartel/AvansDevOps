905ffa69a451d8adba0f1b0fb3625813b70f69de
Wouter-Klaassen <94183945+Wouter-Klaassen@users.noreply.github.com>
2024-06-23 17:54:12 +0200

    fixed small bug

--- Application.Services/obj/Debug/net6.0/Application.Services.AssemblyInfo.cs
+++ Application.Services/obj/Debug/net6.0/Application.Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ef181d80254f4d0663bcc72dac00a53e5a7741c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae938c6b7061e418d91b5ee8a94d45526b199b32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
